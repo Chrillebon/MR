8b32dfef67cb9eaa07940b4d43c128e2d47923d2
s194323 <s194323@student.dtu.dk>
2020-01-16 14:06:25 +0100

    ny funktion addnoise

--- Calling.m
+++ Calling.m
@@ -5,6 +5,7 @@ close all;
 simData = generate_simdata(256,'C:\Users\aleks\OneDrive\Skole\DTU\1. Semester\Intro til MatTek\3 Ugers\MR Projekt');
 figure;
 imagesc(simData);
+colorbar;
 
 %% Generate phantom image
 phanton_im = phantom(256,256);
@@ -15,6 +16,12 @@ imagesc(phanton_im);
 sim_fourier = fft2(simData);
 figure;
 imagesc(log(abs(fftshift(sim_fourier))));
+
+%% Adds noise to image data
+im_noisy = addnoise(simData, 10);
+figure;
+imagesc(im_noisy);
+colorbar;
 
 %% Invers Fourier transforms image data
 inv_fourier = ifft2(sim_fourier);
