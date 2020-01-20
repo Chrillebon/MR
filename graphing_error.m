e93a1a159d3998a49928e0bfd581bf1a5ed883c0
MarcusGalea <s194336rstudent.dtu.dk>
2020-01-20 09:31:58 +0100

    Marcus ændrning

--- graphing_error.m
+++ graphing_error.m
@@ -1,5 +1,6 @@
 function graph=graphing_error(noise_array,pad_array,simData)
 figure(); hold on
+text=strsplit(num2str(noise_array*100));
 for i=1:length(noise_array)
         sim_fourier = fft2(simData);
         im_noisy = addnoise(sim_fourier, noise_array(i));
@@ -8,11 +9,14 @@ for i=1:length(noise_array)
         inv_fourier = ifft2(ifftshift(signal_pad));
         Error(k)=error_measure(simData,inv_fourier);
     end
+   
     graph=plot(pad_array,Error);
-    xlabel('Pad array');
+    xlabel('Fraction of image made visible');
     ylabel('Error');
 
 end
+lgd=legend({text{1:length(text)}},'Location','north');
+title(lgd,'Percentage of noise');
 hold off
 return 
 
