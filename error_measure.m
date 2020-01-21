550b5c1852f512b5c758c9e2839c0c3a3e604288
Chrillebon <Chrillebon@gmail.com>
2020-01-21 09:47:26 +0100

    Comments to error functions

--- error_measure.m
+++ error_measure.m
@@ -1,6 +1,14 @@
-%%Fejlmål
-function Error=error_measure(simData,inv_fourier)
-Error=(norm((simData-inv_fourier),'fro'))/norm(simData,'fro');
-return
+%% Header
+
+% This funtion is used to calculate the error between a known image and a
+% recreation of that same image, like after a recreation through inverse
+% DFT transformation. The 
+
+%% Function
+function Error=error_measure(known_image, recreated_image)
+
+Error = norm( known_image - recreated_image, 'fro') / norm(known_image, 'fro');
+
+end
 
 
--- graphing_error.m
+++ graphing_error.m
@@ -1,20 +1,47 @@
+%% Header
+
+% This function creates a graph over how the error in recreating images
+% changes with added noise and changing padding. The two first arrays
+% changes the values of noise and padding used to create the graph. They
+% should be given in range [0,1].
+% "simData" is an image that we DFT tranform to know how the recreation
+% should be.
+
+%% Function
 function graph=graphing_error(noise_array,pad_array,simData)
 figure(); hold on
+
+% Text for naming and displaying different noise levels
 text=strsplit(num2str(noise_array*100));
+
+% DFT transform original image
+sim_fourier = ft2(simData);
+
+% One line for every level of noise
 for i=1:length(noise_array)
-        sim_fourier = ft2(simData);
-        im_noisy = addnoise(sim_fourier, noise_array(i));
+    
+    % Adds noise
+    im_noisy = addnoise(sim_fourier, noise_array(i));
+    
+    % Goes through all levels of padding
     for k=1:length(pad_array)
         signal_pad = signal_limited(im_noisy, pad_array(k));
+        
+        % Recreates the picture
         inv_fourier = ift2(signal_pad);
+        
+        % Calculate error
         Error(k)=error_measure(simData,inv_fourier);
     end
    
+    % Add to graph
     graph=plot(pad_array,Error);
     xlabel('Fraction of image made visible');
     ylabel('Error');
 
 end
+
+% Add text
 lgd=legend({text{1:length(text)}},'Location','north');
 title(lgd,'Percentage of noise');
 hold off
