ef59c4bce316a6446567a1d070d31ecb6af155e6
Chrillebon <Chrillebon@gmail.com>
2020-01-21 14:19:09 +0100

    updated make_MR

--- make_MR_data.m
+++ make_MR_data.m
@@ -6,26 +6,26 @@ clc;
 %% Generate simulated data
 
 % Figures without textures
-simData = generate_simdata(256);
-disp_slice(simData);
+sim_data = generate_simdata(256);
+disp_slice(sim_data);
 
 % Figures with textures
-simDataTex = generate_simdata(256, '<Your Path to texfile goes here!>');
-disp_slice(simDataTex);
+simTex_data = generate_simdata(256, '<Your Path to texfile goes here!>');
+disp_slice(simTex_data);
 
 % Phantom image
-phan = generate_simdata(256, 'phantom');
-disp_slice(phan);
+phan_data = generate_simdata(256, 'phantom');
+disp_slice(phan_data);
 
 %% Fourier transforms image data
 
-sim_fourier = ft2(simData);
+sim_fourier = ft2(sim_data);
 disp_slice(sim_fourier);
 
-simTex_fourier = ft2(simDataTex);
+simTex_fourier = ft2(simTex_data);
 disp_slice(simTex_fourier);
 
-phan_fourier = ft2(phan);
+phan_fourier = ft2(phan_data);
 disp_slice(phan_fourier);
 
 %% Adds noise to image data
@@ -49,3 +49,22 @@ disp_slice(simTex_pad);
 
 phan_pad = signal_limited(phan_noise, 30);
 disp_slice(phan_pad);
+
+%% Reconstruct
+
+sim_rec = ift2(sim_pad);
+disp_slice(sim_rec);
+
+simTex_rec = ift2(simTex_pad);
+disp_slice(simTex_rec);
+
+phan_rec = ift2(phan_pad);
+disp_slice(phan_rec);
+
+%% Error graphing
+
+graphing_error(0:0.2:1,0:0.1:1,sim_data);
+
+graphing_error(0:0.1:1,0:0.2:1,simTex_data);
+
+graphing_error(0:0.05:1,0:0.05:1,phan_data);
